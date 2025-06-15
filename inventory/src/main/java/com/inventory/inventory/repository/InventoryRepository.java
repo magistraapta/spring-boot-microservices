b91dbf7e4c807de96d28fef49bb58ac7e098dd84
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-06-15 13:14:57 +0800

    delete all service

--- inventory/src/main/java/com/inventory/inventory/repository/InventoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.inventory.inventory.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.inventory.inventory.entity.InventoryEntity;
-
-public interface InventoryRepository extends JpaRepository<InventoryEntity, Long> {
-    boolean existsBySkuCodeAndQuantityGreaterThanEqual(String skuCode, Integer quantity);
-}
--- inventory/src/main/resources/db/migration/V1_create_table_inventory.sql
+++
@@ -1,5 +0,0 @@
-create table inventory (
-    id serial primary key,
-    sku_code varchar(255) not null,
-    quantity int not null
-);
