dc290bacf80296702b30793dbe965b4a16e1de3e
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-06-15 11:49:27 +0800

    fix: inventory controller

--- inventory/src/main/java/com/inventory/inventory/controller/InventoryController.java
+++ inventory/src/main/java/com/inventory/inventory/controller/InventoryController.java
@@ -2,7 +2,6 @@ package com.inventory.inventory.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,18 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.inventory.inventory.service.InventoryService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/inventory")
+@RequiredArgsConstructor
 public class InventoryController {
     private final InventoryService inventoryService;
 
-    public InventoryController(InventoryService inventoryService) {
-        this.inventoryService = inventoryService;
-    }
-
-    @GetMapping("/{skuCode}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable String skuCode, @RequestParam int quantity) {
+    public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
         return inventoryService.isInStock(skuCode, quantity);
     }
 }
